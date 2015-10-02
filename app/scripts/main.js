4d0ff601aeaf7a91f45c8f0c54f2956bc661e782
Shunya Matsuno <id.shma@gmail.com>
2015-10-02 11:47:35 +0900

    modify

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,44 @@
+window.onload = function(){
+  $(function() {
+    $("#loading").fadeOut();
+    $("#fullpage").fadeIn();
+  });
+}
+
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        verticalCentered: true 
+        verticalCentered: true,
+        sectionsColor : ['none', '#fff', '#fff'],
+        //autoScrolling: false,
+        scrollOverflow: true,
+        afterLoad: function(anchorLink, index){
+            var loadedSection = $(this);
+
+            if(index == 2){
+              $('.mb-about').show(); 
+              $('.mb-about').addClass('animated bounceInDown'); 
+
+              $('.mb-profile-image').show(); 
+              $('.mb-profile-image').addClass('fadeIn'); 
+
+              $('.mb-p').show(); 
+              $('.mb-p').addClass('fadeIn'); 
+            }
+            if(index == 3){
+              $('.mb-works').show(); 
+              $('.mb-works').addClass('animated bounceInDown'); 
+
+              $('.mb-col-1').show();
+              $('.mb-col-1').addClass('animated flipInX');
+
+              $('.mb-col-2').show();
+               $('.mb-col-2').addClass('animated flipInX'); 
+               
+              $('.mb-col-3').show();
+               $('.mb-col-3').addClass('animated flipInX'); 
+            }
+        }
     });
+
 });
 
