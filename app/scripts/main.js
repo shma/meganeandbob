5e316617b1e233a3eed968a6311af70409aadcc8
Shunya Matsuno <id.shma@gmail.com>
2015-11-27 02:37:50 +0900

    サイトリニューアル

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,43 +1,47 @@
-window.onload = function(){
-  $(function() {
-    $("#loading").fadeOut();
-    $("#fullpage").fadeIn();
-  });
-}
-
 $(document).ready(function() {
-    $('#fullpage').fullpage({
-        verticalCentered: true,
-        sectionsColor : ['none', '#fff', '#fff'],
-        //autoScrolling: false,
-        scrollOverflow: true,
-        afterLoad: function(anchorLink, index){
-            var loadedSection = $(this);
+    var hash =  location.hash;
+    console.log(hash);
+    if (hash == "#about") {
+        $(".mb-about").show();
+        $(".mb-contact").hide();
+        $(".mb-works").hide();
+        $(".mb-kasaneru").hide();
+    } else if (hash == "#works") {
+        $(".mb-works").show();
+        $(".mb-about").hide();
+        $(".mb-contact").hide();
+        $(".mb-kasaneru").hide();
+    } else if (hash == "#contact") {
+        $(".mb-contact").show();
+        $(".mb-about").hide();
+        $(".mb-works").hide();
+        $(".mb-kasaneru").hide();
+    } else if (hash == "#kasaneru") {
+        $(".mb-contact").hide();
+        $(".mb-about").hide();
+        $(".mb-works").hide();
+        $(".mb-kasaneru").show();
+    } else {
+        $(".mb-works").show();
+    }
 
-            if(index == 2){
-              $('.mb-about').show(); 
-              $('.mb-about').addClass('animated bounceInDown'); 
-
-              $('.mb-profile-image').show(); 
-              $('.mb-profile-image').addClass('fadeIn'); 
-
-              $('.mb-p').show(); 
-              $('.mb-p').addClass('fadeIn'); 
-            }
-            if(index == 3){
-              $('.mb-works').show(); 
-              $('.mb-works').addClass('animated bounceInDown'); 
-
-              $('.mb-col-1').show();
-              //$('.mb-col-1').addClass('animated flipInX');
-
-              $('.mb-col-2').show();
-              // $('.mb-col-2').addClass('animated flipInX'); 
-               
-              $('.mb-col-3').show();
-              //$('.mb-col-3').addClass('animated flipInX'); 
-            }
-        }
+    $(".mba").on('click',function() {
+        $(".mb-about").show();
+        $(".mb-contact").hide();
+        $(".mb-works").hide();
+        $(".mb-kasaneru").hide();
+    });
+    $(".mbw").on('click',function() {
+        $(".mb-works").show();
+        $(".mb-about").hide();
+        $(".mb-contact").hide();
+        $(".mb-kasaneru").hide();
+    });
+    $(".mbc").on('click',function() {
+        $(".mb-contact").show();
+        $(".mb-about").hide();
+        $(".mb-works").hide();
+        $(".mb-kasaneru").hide();
     });
 
 });
