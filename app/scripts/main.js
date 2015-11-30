3b1ba38b1c9c80f8a350bb99ada178d20929991f
Shunya Matsuno <id.shma@gmail.com>
2015-11-30 15:50:35 +0900

    バグ修正

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,22 +5,22 @@ $(document).ready(function() {
         $(".mb-about").show();
         $(".mb-contact").hide();
         $(".mb-works").hide();
-        $(".mb-kasaneru").hide();
+        $(".mb-kasaneru-row").hide();
     } else if (hash == "#works") {
         $(".mb-works").show();
         $(".mb-about").hide();
         $(".mb-contact").hide();
-        $(".mb-kasaneru").hide();
+        $(".mb-kasaneru-row").hide();
     } else if (hash == "#contact") {
         $(".mb-contact").show();
         $(".mb-about").hide();
         $(".mb-works").hide();
-        $(".mb-kasaneru").hide();
+        $(".mb-kasaneru-row").hide();
     } else if (hash == "#kasaneru") {
         $(".mb-contact").hide();
         $(".mb-about").hide();
         $(".mb-works").hide();
-        $(".mb-kasaneru").show();
+        $(".mb-kasaneru-row").show();
     } else {
         $(".mb-works").show();
     }
@@ -29,25 +29,25 @@ $(document).ready(function() {
         $(".mb-about").show();
         $(".mb-contact").hide();
         $(".mb-works").hide();
-        $(".mb-kasaneru").hide();
+        $(".mb-kasaneru-row").hide();
     });
     $(".mbw").on('click',function() {
         $(".mb-works").show();
         $(".mb-about").hide();
         $(".mb-contact").hide();
-        $(".mb-kasaneru").hide();
+        $(".mb-kasaneru-row").hide();
     });
     $(".mbc").on('click',function() {
         $(".mb-contact").show();
         $(".mb-about").hide();
         $(".mb-works").hide();
-        $(".mb-kasaneru").hide();
+        $(".mb-kasaneru-row").hide();
     });
     $(".mb-kasaneru").on('click',function() {
         $(".mb-contact").hide();
         $(".mb-about").hide();
         $(".mb-works").hide();
-        $(".mb-kasaneru").show();
+        $(".mb-kasaneru-row").show();
     });
 });
 
